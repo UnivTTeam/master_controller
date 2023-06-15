ad924a27c74be5a9568acef704d56a1a4b8cf56f
Kentaro Seki <kentaro.kpc@gmail.com>
2023-06-15 22:28:11 +0900

    リファクタリング

--- task.cpp
+++ task.cpp
@@ -10,6 +10,8 @@
 
 #define PS4_MAC "08:B6:1F:3B:81:AE"  //PS4コントローラのMACアドレス
 
+using linear::Vec2, linear::Rot2;
+
 namespace Task {
 enum class Mode : int {
   Emergency = -1,
@@ -43,7 +45,7 @@ private:
   int last_time;
 };
 
-linear::Vec2<float> v_dest(0.0f, 0.0f);
+Vec2<float> v_dest(0.0f, 0.0f);
 float theta_dest = Params::init_pos.rot.getAngle();
 float omega_dest = 0.0f;
 
@@ -61,113 +63,131 @@ void setupTask() {
   PS4.begin(PS4_MAC);
 }
 
-float stickToVelocity(int8_t stick_input){
-  float ratio = static_cast<float>(stick_input) / 128.0f;
+float readStickRaw(int8_t x) {
+  float ratio = static_cast<float>(x) / 128.0f;
   if(abs(ratio) < 0.1f){
     ratio = 0.0f;
   }
-  return Params::MAX_PARA_VEL * ratio;
+  return ratio;
+}
+Vec2<float> readStick() {
+  return Vec2<float>(
+    readStickRaw(PS4.LStickX()),
+    readStickRaw(PS4.RStickY()),
+  );
+}
+bool isStickInterrupt() {
+  Vec2<float> stick = readStick();
+  return (stick.x != 0.0f) | (stick.y != 0.0f);
+}
+
+void setAutoRot(float duration, float dTheta){
+  mode = Mode::Auto;
+
+  float start_time = current_time;
+  float theta0 = theta_dest;
+  auto_mode_callback = [&, duration, dTheta, start_time, theta0](){
+    float ratio = (current_time - start_time) / duration;
+    theta_dest = theta_dest0 +  dThta * min(ratio, 1.0f);
+    omega_dest = dTheta / duration;
+    return (ratio >= 1.0f);
+  };
+}
+
+void setAutoPara(float duration, const Vec2<float>& dr){
+  mode = Mode::Auto;
+
+  float start_time = current_time;
+  Vec2<float> init_pos = robot_pos.static_frame.pos;
+  auto_mode_callback = [&, start_time, duration, dr, init_pos](){
+    float ratio = min((current_time - start_time) / duration, 1.0f);
+    auto dest_pos = init_pos + ratio * dr;
+
+    float Kp = 5.0f;
+    auto fb_vel = -Kp * (robot_pos.static_frame.pos - dest_pos);
+
+    auto ff_vel = Vec2<float>(0.0f, 0.0f);
+    if(ratio < 1.0f){
+      ff_vel = dr / duration;
+    }
+
+    v_dest = ff_vel + fb_vel;
+
+    return isStickInterrupt;
+  };
+}
+
+void setAutoGTGT(){
+  mode = Mode::Auto;
+
+  float start_time = current_time;
+  auto_mode_callback = [&, start_time](){
+    float t = current_time - start_time;
+    int T = t;
+    if(T%2 == 0){
+      v_dest.x = -500.0f;
+    }else{
+      v_dest.x = 450.0f;
+    }
+    return isStickInterrupt();
+  };
 }
 
 void taskCallback() {
   current_time = micros() / (1000.0f * 1000.0f);
   // 緊急停止処理
-  if(PS4.Touchpad()){
+  bool emergency_button = PS4.Cross();
+  if(emergency_button){
     mode = Mode::Emergency;
   }
   if(mode == Mode::Emergency){
     CommandValue::wheel_vx = 0.0f;
     CommandValue::wheel_vy = 0.0f;
     CommandValue::wheel_vw = 0.0f;
-    if(!PS4.Touchpad()){
+    if(!emergency_button){
       mode = Mode::Manual;
     }
   }
   
   if(mode != Mode::Emergency){
     // 熊手処理
-    if(kumade_wrapper(PS4.Circle())){
-      if(elevator_state < 4){
-        Serial.printf("熊手を上げる処理 %d\n", elevator_state);
-        for(const auto& pin : Params::ELEVATOR_PIN[elevator_state]) {
-          pinMode(pin, OUTPUT);
-          digitalWrite(pin, LOW);
-        }
-        elevator_state++;
+    if(kumade_wrapper(PS4.Up())){
+      elevator_state++;
+      Serial.printf("熊手上昇指令 %d\n", elevator_state);
+    }
+    using Params::ELEVATOR_PIN;
+    for(int i=0; i<ELEVATOR_PIN.size(); i++){
+      bool tf = (i < elevator_state);
+      for(const auto& pin : ELEVATOR_PIN[i]){
+        digitalWrite(pin, tf);
       }
     }
 
     // 足回り処理
-    v_dest = linear::Vec2<float>(0.0f, 0.0f);
+    v_dest = Vec2<float>(0.0f, 0.0f);
     omega_dest = 0.0f;
     if(mode == Mode::Manual) {
       // スティック入力
-      v_dest.x = stickToVelocity(PS4.LStickX());
-      v_dest.y = stickToVelocity(PS4.RStickY());
+      v_dest = Params::MAX_PARA_VEL * readStick();
 
       // L1 R1による角度微調整
       if (l1_wrapper(PS4.L1())) {
         float dtheta = Params::l1r1_rot_angle;
-        robot_pos.static_frame.rot = linear::Rot2<float>(robot_pos.static_frame.rot.getAngle() + dtheta);
+        robot_pos.static_frame.rot = Rot2<float>(robot_pos.static_frame.rot.getAngle() + dtheta);
       }
       if (r1_wrapper(PS4.R1())) {
         float dtheta = -Params::l1r1_rot_angle;
-        robot_pos.static_frame.rot = linear::Rot2<float>(robot_pos.static_frame.rot.getAngle() + dtheta);
+        robot_pos.static_frame.rot = Rot2<float>(robot_pos.static_frame.rot.getAngle() + dtheta);
       }
 
       // モード受付
       if (PS4.L2()) { // L2回転
-        mode = Mode::Auto;
-        float start_time = current_time;
-        float theta_dest0 = theta_dest;
-        auto_mode_callback = [&, start_time, theta_dest0](){
-          float r = (current_time - start_time) / Params::l2r2_rot_time;
-          float dThta = Params::l2r2_rot_angle;
-          theta_dest = theta_dest0 +  dThta * min(r, 1.0f);
-          omega_dest = dThta / Params::l2r2_rot_time;
-          return (r >= 1.0f);
-        };
+        setAutoRot(Params::l2r2_rot_time, Params::l2r2_rot_angle);
       } else if (PS4.R2()) { // R2回転
-        mode = Mode::Auto;
-        float start_time = current_time;
-        float theta_dest0 = theta_dest;
-        auto_mode_callback = [&, start_time, theta_dest0](){
-          float r = (current_time - start_time) / Params::l2r2_rot_time;
-          float dThta = -Params::l2r2_rot_angle;
-          theta_dest = theta_dest0 +  dThta * min(r, 1.0f);
-          omega_dest = dThta / Params::l2r2_rot_time;
-          return (r >= 1.0f);
-        };
-      } else if (PS4.Cross()) {
-        mode = Mode::Auto;
-        float start_time = current_time;
-        auto_mode_callback = [&, start_time](){
-          float t = current_time - start_time;
-          v_dest.x = 0.2 * std::sin(10.0f * t);
-          return (t >= 1.0f);
-        };
+        setAutoRot(Params::l2r2_rot_time, -Params::l2r2_rot_angle);
       } else if (PS4.Triangle()) {
-        mode = Mode::Auto;
-        float start_time = current_time;
-        float duration_time = 4.0f;
-        linear::Vec2<float> dr = linear::Vec2<float>{1765, 1765};
-        linear::Vec2<float> init_pos = robot_pos.static_frame.pos;
-        auto_mode_callback = [&, start_time, duration_time, dr, init_pos](){
-          float ratio = min((current_time - start_time) / duration_time, 1.0f);
-          auto dest_pos = init_pos + ratio * dr;
-          float Kp = 5.0f;
-          auto fb_vel = -Kp * (robot_pos.static_frame.pos - dest_pos);
-
-          if(ratio < 1.0f){
-            v_dest = dest_pos / duration_time + fb_vel;
-          }else{
-            if (PS4.Triangle()){
-              return true;
-            }
-            v_dest = fb_vel;
-          }
-          return false;
-        };
+        setAutoGTGT();
+      } else if (PS4.Circle()) {
       }
     } else if (mode == Mode::Auto){
       if(auto_mode_callback()){
@@ -182,7 +202,6 @@ void taskCallback() {
     );
 
     setVelocityFromField(v_dest.x, v_dest.y, theta_dest);
-
   }
 
   /*
