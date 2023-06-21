2a813c78fbf237915fa60a4925ad21d29492c3ef
Kentaro Seki <kentaro.kpc@gmail.com>
2023-06-21 19:25:17 +0900

    fixed typo

--- elevator.cpp
+++ elevator.cpp
@@ -12,14 +12,12 @@ volatile int elevator_step = 0;
 
 void elevatorCallback()
 {
-  if(current_time < end_time){
-    for(int i=0; i<Params::ELEVATOR_PINS.size(); i++){
-      const auto pins = Params::ELEVATOR_PINS[i];
-      float t = current_time - start_time;
-      for(int j=0; j<pins.size(); j++){
-        int k = t / Params::ELEVATOR_TIME;
-        digitalWrite(pins[j], (i==target_pin)&(j==k));
-      }
+  for(int i=0; i<Params::ELEVATOR_PINS.size(); i++){
+    const auto pins = Params::ELEVATOR_PINS[i];
+    float t = current_time - start_time;
+    for(int j=0; j<pins.size(); j++){
+      int k = t / Params::ELEVATOR_TIME;
+      digitalWrite(pins[j], (i==target_pin)&(j==k));
     }
   }
 }
